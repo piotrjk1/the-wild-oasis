e71e3f5a8d891bfd68ed1d1e83ab48847e02e6d9
Piotr Krzyzak <piotrjakubkrzyzak@gmail.com>
2025-09-12 13:50:22 +0200

    Update

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://orneitftolmkgbuymtan.supabase.co";
+export const supabaseUrl = "https://orneitftolmkgbuymtan.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ybmVpdGZ0b2xta2didXltdGFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0NTI1ODcsImV4cCI6MjA3MzAyODU4N30.v96yHupbjsbpHNx1HSdY0JR62LB-WisNt4TK3Nfneh4";
 const supabase = createClient(supabaseUrl, supabaseKey);
